6316d35e41c9dd193ff00b676c3466055e1afb1c
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-27 18:58:57 +0600

    last update 11/27/2022 - 6:58

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 /*=============== SHOW MENU ===============*/
+const navManu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navManu.classList.add("show-menu");
+  });
+}
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navManu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
